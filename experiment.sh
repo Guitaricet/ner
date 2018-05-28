0d1b369950d5fc39ff633937d87fd339cd98b65a
Vladislav Lyalin <guitaricet@gmail.com>
2018-05-28 10:20:23 +0300

    fixes for experiment sh

--- experiment.sh
+++ experiment.sh
@@ -9,12 +9,12 @@ python3 noise_experiment.py --dataset collection5 --network-type gru --embedding
 python3 noise_experiment.py --dataset collection5 --network-type gru --no-char-embeddings --results-filename collection5_noembed_nochar_grunet.csv
 python3 noise_experiment.py --dataset collection5 --network-type gru --no-char-embeddings --embeddings /media/data/nlp/data/wiki.en.bin --results-filename collection5_fasttext_nochar_grunet.csv
 # echo "\nCONLL\n"
-# python3 noise_experiment.py --dataset collection5 --results-filename collection5_noembed.csv
-# python3 noise_experiment.py --dataset collection5 --embeddings /media/data/nlp/data/wiki.en.bin --results-filename collection5_fasttext_nochar.csv
-# python3 noise_experiment.py --dataset collection5 --no-char-embeddings --results-filename collection5_noembed_nochar.csv
-# python3 noise_experiment.py --dataset collection5 --no-char-embeddings --results-filename --embeddings /media/data/nlp/data/wiki.en.bin collection5_fasttext_nochar.csv
+# python3 noise_experiment.py --dataset conll2003 --results-filename conll2003_noembed.csv
+# python3 noise_experiment.py --dataset conll2003 --embeddings /media/data/nlp/data/wiki.en.bin --results-filename conll2003_fasttext_nochar.csv
+# python3 noise_experiment.py --dataset conll2003 --no-char-embeddings --results-filename conll2003_noembed_nochar.csv
+# python3 noise_experiment.py --dataset conll2003 --no-char-embeddings --results-filename --embeddings /media/data/nlp/data/wiki.en.bin conll2003_fasttext_nochar.csv
 echo "\nCONLL GRU network\n"
-python3 noise_experiment.py --dataset collection5 --network-type gru --results-filename collection5_noembed_grunet.csv
-python3 noise_experiment.py --dataset collection5 --network-type gru --embeddings /media/data/nlp/data/wiki.en.bin --results-filename collection5_fasttext_grunet.csv
-python3 noise_experiment.py --dataset collection5 --network-type gru --no-char-embeddings --results-filename collection5_noembed_nochar_grunet.csv
-python3 noise_experiment.py --dataset collection5 --network-type gru --no-char-embeddings --embeddings /media/data/nlp/data/wiki.en.bin --results-filename collection5_fasttext_nochar_grunet.csv
+python3 noise_experiment.py --dataset conll2003 --network-type gru --results-filename conll2003_noembed_grunet.csv
+python3 noise_experiment.py --dataset conll2003 --network-type gru --embeddings /media/data/nlp/data/wiki.en.bin --results-filename conll2003_fasttext_grunet.csv
+python3 noise_experiment.py --dataset conll2003 --network-type gru --no-char-embeddings --results-filename conll2003_noembed_nochar_grunet.csv
+python3 noise_experiment.py --dataset conll2003 --network-type gru --no-char-embeddings --embeddings /media/data/nlp/data/wiki.en.bin --results-filename conll2003_fasttext_nochar_grunet.csv
