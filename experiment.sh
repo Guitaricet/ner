df7726c65134ff328061999e1f36faa4a04a61c4
Vladislav Lyalin <guitaricet@gmail.com>
2018-05-28 13:14:51 +0300

    fixes

--- experiment.sh
+++ experiment.sh
@@ -1,8 +1,8 @@
 echo -e "\nCollection5\n"
-python3 noise_experiment.py --dataset collection5 --results-filename collection5_noembed.csv
-python3 noise_experiment.py --dataset collection5 --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename collection5_fasttext_nochar.csv
+# python3 noise_experiment.py --dataset collection5 --results-filename collection5_noembed.csv
+python3 noise_experiment.py --dataset collection5 --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename collection5_fasttext.csv
 python3 noise_experiment.py --dataset collection5 --no-char-embeddings --results-filename collection5_noembed_nochar.csv
-python3 noise_experiment.py --dataset collection5 --no-char-embeddings --results-filename --embeddings /media/data/nlp/data/wiki.ru.bin collection5_fasttext_nochar.csv
+python3 noise_experiment.py --dataset collection5 --no-char-embeddings --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename collection5_fasttext_nochar.csv
 echo -e "\nCollection5 GRU network\n"
 python3 noise_experiment.py --dataset collection5 --network-type gru --results-filename collection5_noembed_grunet.csv
 python3 noise_experiment.py --dataset collection5 --network-type gru --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename collection5_fasttext_grunet.csv
