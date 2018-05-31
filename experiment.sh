83841b36c09cf478483671e0a9baecc1fbeeca56
Vladislav Lyalin <guitaricet@gmail.com>
2018-05-31 20:57:52 +0300

    updated experiment.sh

--- experiment.sh
+++ experiment.sh
@@ -3,9 +3,9 @@
 # python3 noise_experiment.py --dataset data/collection5 --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext.csv
 # python3 noise_experiment.py --dataset data/collection5 --no-char-embeddings --results-filename results/collection5_noembed_nochar.csv
 # python3 noise_experiment.py --dataset data/collection5 --no-char-embeddings --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_nochar.csv
-echo -e "\nCollection5 Fixed Word2Vec\n"
-python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin --results-filename results/collection5_word2vec_fixed.csv
-python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin  --no-char-embeddings --results-filename results/collection5_word2vec_fixed_nochar.csv
+# echo -e "\nCollection5 Fixed Word2Vec\n"
+# python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin --results-filename results/collection5_word2vec_fixed.csv
+# python3 noise_experiment.py --ataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin  --no-char-embeddings --results-filename results/collection5_word2vec_fixed_nochar.csv
 # echo -e "\nCollection5 GRU network\n"
 # python3 noise_experiment.py --dataset data/collection5 --network-type gru --results-filename results/collection5_noembed_grunet.csv
 # python3 noise_experiment.py --dataset data/collection5 --network-type gru --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_grunet.csv
@@ -21,3 +21,6 @@ python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2
 # python3 noise_experiment.py --dataset data/conll2003 --network-type gru --embeddings /media/data/nlp/data/wiki.en.bin --results-filename results/conll2003_fasttext_grunet.csv
 # python3 noise_experiment.py --dataset data/conll2003 --network-type gru --no-char-embeddings --results-filename results/conll2003_noembed_nochar_grunet.csv
 # python3 noise_experiment.py --dataset data/conll2003 --network-type gru --no-char-embeddings --embeddings /media/data/nlp/data/wiki.en.bin --results-filename results/conll2003_fasttext_nochar_grunet.csv
+echo -e "\nCONLL Fixed Word2Vec\n"
+python3 noise_experiment.py --dataset data/conll2003 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin --results-filename results/conll_word2vec_fixed.csv
+python3 noise_experiment.py --ataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/GoogleNews-vectors-negative300.bin  --no-char-embeddings --results-filename results/conll_word2vec_fixed_nochar.csv
