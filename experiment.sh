0b47d88a5931b03894da2e70e7f93a83e952f421
Vladislav Lyalin <guitaricet@gmail.com>
2018-06-09 17:14:42 +0300

    lstm for collection 5 and CAp

--- experiment.sh
+++ experiment.sh
@@ -8,11 +8,11 @@
 # python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/ruwikiruscorpora-nobigrams_upos_skipgram_300_5_2018.vec --postag --results-filename results/collection5_word2vec_fixed.csv
 # python3 noise_experiment.py --dataset data/collection5 --embeddings-format word2vec --embeddings /media/data/nlp/data/ruwikiruscorpora-nobigrams_upos_skipgram_300_5_2018.vec --postag  --no-char-embeddings --results-filename results/collection5_word2vec_fixed_nochar.csv
 
-# echo -e "\nCollection5 LSTM network\n"
-# python3 noise_experiment.py --dataset data/collection5 --network-type lstm --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_lstmnet.csv
-# python3 noise_experiment.py --dataset data/collection5 --network-type lstm --no-char-embeddings --results-filename results/collection5_noembed_nochar_lstmnet.csv
-# python3 noise_experiment.py --dataset data/collection5 --network-type lstm --no-char-embeddings --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_nochar_lstmnet.csv
-# python3 noise_experiment.py --dataset data/collection5 --network-type lstm --results-filename results/collection5_noembed_lstmnet.csv
+echo -e "\nCollection5 LSTM network\n"
+python3 noise_experiment.py --dataset data/collection5 --network-type lstm --results-filename results/collection5_noembed_lstmnet.csv
+python3 noise_experiment.py --dataset data/collection5 --network-type lstm --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_lstmnet.csv
+python3 noise_experiment.py --dataset data/collection5 --network-type lstm --no-char-embeddings --results-filename results/collection5_noembed_nochar_lstmnet.csv
+python3 noise_experiment.py --dataset data/collection5 --network-type lstm --no-char-embeddings --embeddings /media/data/nlp/data/wiki.ru.bin --results-filename results/collection5_fasttext_nochar_lstmnet.csv
 
 # echo -e "\nFactRU\n"
 # python3 noise_experiment.py --epochs 30 --dataset data/factru --results-filename results/factru_noembed_30epochs.csv
@@ -47,9 +47,15 @@
 # python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --not-trainable-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable.csv
 # python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --not-trainable-embeddings --no-char-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable_nochar.csv
 
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_original.csv --noise 0
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --no-char-embeddings --results-filename results/CAp_noembed_nochar_original.csv --noise 0
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --embeddings /media/data/nlp/data/cc.fr.300.bin --results-filename results/CAp_fasttext_original.csv --noise 0
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --embeddings /media/data/nlp/data/cc.fr.300.bin --no-char-embeddings --results-filename results/CAp_fasttext_nochar_original.csv --noise 0
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --not-trainable-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable_original.csv --noise 0
-python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --not-trainable-embeddings --no-char-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable_nochar_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --no-char-embeddings --results-filename results/CAp_noembed_nochar_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --embeddings /media/data/nlp/data/cc.fr.300.bin --results-filename results/CAp_fasttext_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --original-testset data/CAp/test_set_wlabels.txt --embeddings /media/data/nlp/data/cc.fr.300.bin --no-char-embeddings --results-filename results/CAp_fasttext_nochar_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --not-trainable-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable_original.csv --noise 0
+# python3 noise_experiment_CAp.py --dataset data/CAp/dataset_original_dict.pkl --not-trainable-embeddings --no-char-embeddings --original-testset data/CAp/test_set_wlabels.txt --results-filename results/CAp_noembed_not_trainable_nochar_original.csv --noise 0
+
+echo -e "\nCAp lstm network\n"
+python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --network-type lstm --results-filename results/CAp_noembed_lstmnet.csv
+python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --network-type lstm --no-char-embeddings --results-filename results/CAp_noembed_nochar_lstmnet.csv
+python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --network-type lstm --embeddings /media/data/nlp/data/wiki.en.bin --results-filename results/CAp_fasttext_lstmnet.csv
+python3 noise_experiment_CAp.py --dataset data/CAp/dataset_dict.pkl --network-type lstm --no-char-embeddings --embeddings /media/data/nlp/data/wiki.en.bin --results-filename results/CAp_fasttext_nochar_lstmnet.csv
